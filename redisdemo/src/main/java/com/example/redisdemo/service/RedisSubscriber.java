12d5c846983bdef34ee99f22c4171525622fc6ac
CharlieBrownyc <brownyc6@gmail.com>
2025-06-20 02:44:25 +0900

    add publish broadcast to redisdemo

--- redisdemo/src/main/java/com/example/redisdemo/service/RedisSubscriber.java
+++ redisdemo/src/main/java/com/example/redisdemo/service/RedisSubscriber.java
@@ -1,11 +1,20 @@
 package com.example.redisdemo.service;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class RedisSubscriber {
+
+    private final RedisPublisher publisher;
 
     public void receiveMessage(String msg) {
         System.out.println("Received via Redis Pub/Sub: " + msg);
+
+        String broadcastMsg = "[broadcast] ";
+        if(!msg.startsWith(broadcastMsg)){
+            publisher.publish("chat", broadcastMsg + msg);
+        }
     }
 }
